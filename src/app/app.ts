7bcfac1848c75f7893b6182d2925a718da4f68cc
Tonyfilho <oficinadosdrones@gmail.com>
2025-11-14 19:20:56 +0000

    tags principais no app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./componentes/header/header";
+import { Main } from "./componentes/main/main";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Main],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
